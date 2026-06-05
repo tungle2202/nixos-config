183704bd0e83d33265818ebbcdf4af828c4800cb
Le Son Tung <tung.ls110106@gmail.com>
2026-06-06 03:33:11 +0700

    fix: syntax

--- hosts/my-pc/configuration.nix
+++ hosts/my-pc/configuration.nix
@@ -47,14 +47,14 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
-  service.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
   	kate
 	okular
 	konsole
 	elisa
 	gwenview
-  ]
+  ];
   # services.xserver.desktopManager.gnome.enable = true;
 
 
