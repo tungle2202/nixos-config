7f7708a3d4459c71fa3e69b3fb7297c5b971fcfe
Le Son Tung <tung.ls110106@gmail.com>
2026-06-06 02:44:42 +0700

    REAL fix: var undefined alerts when rebuilding

--- flake.nix
+++ flake.nix
@@ -7,10 +7,9 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager... }: {
     nixosConfigurations.my-pc = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
       modules = [
 	./hosts/my-pc/configuration.nix
 	home-manager.nixosModules.home-manager
