3b3aca88bed7687658c8164c2f35dea13a6cb7cd
Le Son Tung <tung.ls110106@gmail.com>
2026-06-06 02:34:50 +0700

    Config: customized bash ui

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }: {
@@ -10,6 +12,11 @@
       system = "x86_64-linux";
       modules = [
         ./hosts/my-pc/configuration.nix
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	}
       ];
     };
   };
--- hosts/my-pc/configuration.nix
+++ hosts/my-pc/configuration.nix
@@ -90,6 +90,21 @@
   home-manager.useUserPackages = true;
   home-manager.users."tungle" = {pkgs, ... }: {
   	home.stateVersion = "26.05";
+
+	programs.bash = {
+		enable = true;
+    		bashrcExtra = ''
+			parse_git_branch() {
+  				git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+			}
+			export PROMPT_COLOR='32m'
+			export DIR_COLOR='33m'
+			export GIT_COLOR='36m'
+
+			export PS1="\n\[\033[$PROMPT_COLOR\]\u@\h:\[\033[$DIR_COLOR\]\w\[\033[$GIT_COLOR\]\$(parse_git_branch)\[\033[0m\]\$ " 
+		 '';
+  	};
+
   };
 
   # Install firefox.
