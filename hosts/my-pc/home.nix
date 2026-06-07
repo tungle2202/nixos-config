83caabcb1216478428fb5d199e244c6438264dd4
Le Son Tung <tung.ls110106@gmail.com>
2026-06-08 01:37:07 +0700

    i hate this whole thing

--- hosts/my-pc/home.nix
+++ hosts/my-pc/home.nix
@@ -4,74 +4,84 @@
 	imports = [
 		inputs.catppuccin.homeModules.catppuccin
 	];
-	home.username = "tungle";
-	home.homeDirectory = "/home/tungle";
-  	home.stateVersion = "26.05";
-	home.packages = with pkgs; [
-		catppuccin-cursors.frappeBlue
-		papirus-icon-theme
-		catppuccin-kde
-	];
+	home = {
+		username = "tungle";
+		homeDirectory = "/home/tungle";
+  		stateVersion = "26.05";
+		packages = with pkgs; [
+			catppuccin-cursors.frappeBlue
+			papirus-icon-theme
+			catppuccin-kde
+		];
+		file.".config/plasma-org.kde.plasma.desktop-appletsrc" = {
+			source = ../.././assets/plasma-org.kde.plasma.desktop-appletsrc;
+ 			onChange = ''
+    				${pkgs.procps}/bin/pkill plasmashell || true
+    				${pkgs.kdePackages.plasma-workspace}/bin/plasmashell &
+  			'';
+		};
+	};
+
+
 
 	catppuccin = {
 		enable = true;
 		flavor = "frappe";
 	};
 
-	programs.kitty = {
-		enable = true;
-		themeFile = "Catppuccin-Frappe";	
-		settings = {
-			font_family = "JetBrainsMono Nerd Font";
-			font_size = "12";
-		};
-	};
-
-	programs.plasma = {
-		enable = true;
-		workspace = {
-			lookAndFeel = "Catppuccin-Frappe";
-			iconTheme = "Papirus-Dark";
-			cursor.theme = "Catppuccin-Frappe-Blue";
-		};
-		configFile.kwinrc = {
-			Plugins = {
-				wobblywindowsEnabled = true;
-				magiclampEnabled = true;
-				windowgeometryEnabled = true;
-			};
-			Windows = {
-				CornerRadius = 3; 
+	programs = {
+		kitty = {
+			enable = true;
+			themeFile = "Catppuccin-Frappe";	
+			settings = {
+				font_family = "JetBrainsMono Nerd Font";
+				font_size = "12";
 			};
 		};
-	};
 
-	programs.bash = {
-		enable = true;
-    		bashrcExtra = ''
-			parse_git_branch() {
-     				git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-			}
+		plasma = {
+			enable = true;
+			configFile = {
+				kwinrc = {
+					Plugins = {
+						wobblywindowsEnabled = true;
+						magiclampEnabled = true;
+						windowgeometryEnabled = true;
+					};
+					Windows = {
+						CornerRadius = 3; 
+					};
+				};
+			};
+		};
 
-			export PROMPT_COLOR='32m'
-			export DIR_COLOR='33m'
-			export GIT_COLOR='36m'
-			export PS1="\n\[\033[$PROMPT_COLOR\]\u@\h:\[\033[$DIR_COLOR\]\w\[\033[$GIT_COLOR\] \$(parse_git_branch)\[\033[0m\]\$ " 
-		 '';
-  	};
+		bash = {
+			enable = true;
+    			bashrcExtra = ''
+				parse_git_branch() {
+     					git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+				}
 
-	programs.neovim = {
-		enable = true;
-		defaultEditor = true;
+				export PROMPT_COLOR='32m'
+				export DIR_COLOR='33m'
+				export GIT_COLOR='36m'
+				export PS1="\n\[\033[$PROMPT_COLOR\]\u@\h:\[\033[$DIR_COLOR\]\w\[\033[$GIT_COLOR\] \$(parse_git_branch)\[\033[0m\]\$ " 
+			 '';
+	  	};
+
+		neovim = {
+			enable = true;
+			defaultEditor = true;
 		
-		initLua = ''
-			vim.opt.number = true
-			vim.opt.relativenumber = true
+			initLua = ''
+				vim.opt.number = true
+				vim.opt.relativenumber = true
 
-			vim.opt.timeoutlen = 500
-		'';
+				vim.opt.timeoutlen = 500
+			'';
+		};
+
+		home-manager.enable = true;
 	};
-
-	programs.home-manager.enable = true;
 }
 
